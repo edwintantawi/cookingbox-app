74ff9a59abaed0334accc6a5cdf890e61019d906
edwintantawi <edwintantawi@gmail.com>
2022-03-13 12:59:52 +0700

    🚀 Create colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:cookingbox_app/utils/create_material_color.dart';
+
+class AppColor {
+  static MaterialColor primaryColor =
+      createMaterialColor(const Color(0xFFF45F36));
+}
