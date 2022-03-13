4830909ee10e44be32175b555fa9b616eb9cf10c
edwintantawi <edwintantawi@gmail.com>
2022-03-13 16:17:55 +0700

    🚀 Create AppShell widget

---
+++ lib/widgets/app_shell.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AppShell extends StatelessWidget {
+  final Widget title;
+  final Widget body;
+
+  const AppShell({
+    Key? key,
+    required this.title,
+    required this.body,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: title,
+        elevation: 0,
+      ),
+      body: body,
+    );
+  }
+}
