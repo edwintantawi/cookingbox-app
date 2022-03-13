417038877a75af3f14f3f55e4917876faf599d4f
edwintantawi <edwintantawi@gmail.com>
2022-03-13 16:19:00 +0700

    ✨ Add navigation to each category

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,55 +1,83 @@
+import 'package:cookingbox_app/screens/meal_screen.dart';
+import 'package:cookingbox_app/widgets/app_shell.dart';
 import 'package:flutter/material.dart';
-import 'package:cookingbox_app/domains/food_category.dart';
+import 'package:cookingbox_app/domains/meal_category.dart';
 import 'package:cookingbox_app/services/the_meal_db_api.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key, required this.title}) : super(key: key);
-
   final String title;
+
+  const HomeScreen({Key? key, required this.title}) : super(key: key);
 
   @override
   _HomeScreen createState() => _HomeScreen();
 }
 
 class _HomeScreen extends State<HomeScreen> {
-  late Future<List<FoodCategory>> foodCategories;
+  late Future<List<MealCategory>> mealCategories;
 
   @override
   void initState() {
     super.initState();
-    foodCategories = fetchCategories();
+    mealCategories = fetchMealCategories();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        elevation: 0,
-      ),
-      body: FutureBuilder<List<FoodCategory>>(
-        future: foodCategories,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.separated(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              itemCount: snapshot.data!.length,
-              separatorBuilder: (BuildContext context, int index) =>
-                  const Divider(),
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text(snapshot.data![index].title),
-                  leading:
-                      Image.network(snapshot.data![index].image, width: 60),
-                );
-              },
-            );
-          } else if (snapshot.hasError) {
-            return Text('${snapshot.error}');
-          }
+    return AppShell(
+      title: Text(widget.title),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Center(
+              child: FutureBuilder<List<MealCategory>>(
+                future: mealCategories,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data == null) {
+                      return const Text('Oops its empty...');
+                    }
 
-          return const CircularProgressIndicator();
-        },
+                    return ListView.separated(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      itemCount: snapshot.data!.length,
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const Divider();
+                      },
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          visualDensity: VisualDensity.comfortable,
+                          trailing: const Icon(Icons.arrow_right_outlined),
+                          iconColor: Theme.of(context).primaryColor,
+                          title: Text(snapshot.data![index].title),
+                          leading: Image.network(
+                            snapshot.data![index].image,
+                            width: 60,
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => MealsScreen(
+                                  category: snapshot.data![index],
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('${snapshot.error}');
+                  }
+
+                  return const CircularProgressIndicator();
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
