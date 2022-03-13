93dd6bd9237af1ba197b6f0885d6cb303585e0bb
edwintantawi <edwintantawi@gmail.com>
2022-03-13 16:17:41 +0700

    🚀 Add featchMealsByCategory

--- lib/services/the_meal_db_api.dart
+++ lib/services/the_meal_db_api.dart
@@ -1,21 +1,40 @@
 import 'dart:convert';
+import 'package:cookingbox_app/domains/meal.dart';
 import 'package:http/http.dart' as http;
-import 'package:cookingbox_app/domains/food_category.dart';
+import 'package:cookingbox_app/domains/meal_category.dart';
 import 'package:cookingbox_app/constants/api_endpoint.dart';
 
-Future<List<FoodCategory>> fetchCategories() async {
+Future<List<MealCategory>> fetchMealCategories() async {
   final response = await http.get(Uri.parse(ApiEndpoint.categories));
 
   if (response.statusCode == 200) {
-    List<FoodCategory> foodCategories = [];
+    List<MealCategory> mealCategories = [];
     final parsed = jsonDecode(response.body);
 
     for (var category in parsed['categories']) {
-      foodCategories.add(FoodCategory.fromJson(category));
+      mealCategories.add(MealCategory.fromJson(category));
     }
 
-    return foodCategories;
+    return mealCategories;
   } else {
     throw Exception('Failed to load categories');
+  }
+}
+
+Future<List<Meal>> featchMealsByCategory(String category) async {
+  final endpoint = '${ApiEndpoint.filter}?c=$category';
+  final response = await http.get(Uri.parse(endpoint));
+
+  if (response.statusCode == 200) {
+    List<Meal> meals = [];
+    final parsed = jsonDecode(response.body);
+
+    for (var meal in parsed['meals']) {
+      meals.add(Meal.fromJson(meal));
+    }
+
+    return meals;
+  } else {
+    throw Exception('Failed to load meals by category');
   }
 }
