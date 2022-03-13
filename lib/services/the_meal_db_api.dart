2532bb9d1bcdd72b6b178b046b4fc5c1ac2d7223
edwintantawi <edwintantawi@gmail.com>
2022-03-13 22:50:40 +0700

    ⚗️ Use dio http with cache

--- lib/services/the_meal_db_api.dart
+++ lib/services/the_meal_db_api.dart
@@ -1,18 +1,18 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
+import 'package:cookingbox_app/utils/http.dart';
 import 'package:cookingbox_app/domains/meal.dart';
 import 'package:cookingbox_app/domains/meal_category.dart';
 import 'package:cookingbox_app/constants/api_endpoint.dart';
 
 Future<List<MealCategory>> fetchMealCategories() async {
-  final response = await http.get(Uri.parse(ApiEndpoint.categories));
+  final response = await http.get(
+    ApiEndpoint.categories,
+    options: cacheOptions,
+  );
 
   if (response.statusCode == 200) {
     List<MealCategory> mealCategories = [];
-    final parsed = jsonDecode(response.body);
 
-    for (var category in parsed['categories']) {
+    for (var category in response.data['categories']) {
       mealCategories.add(MealCategory.fromJson(category));
     }
 
@@ -24,13 +24,15 @@ Future<List<MealCategory>> fetchMealCategories() async {
 
 Future<List<Meal>> fetchMealsByCategory(String category) async {
   final endpoint = '${ApiEndpoint.filter}?c=$category';
-  final response = await http.get(Uri.parse(endpoint));
+  final response = await http.get(
+    endpoint,
+    options: cacheOptions,
+  );
 
   if (response.statusCode == 200) {
     List<Meal> meals = [];
-    final parsed = jsonDecode(response.body);
 
-    for (var meal in parsed['meals']) {
+    for (var meal in response.data['meals']) {
       meals.add(Meal.fromJson(meal));
     }
 
